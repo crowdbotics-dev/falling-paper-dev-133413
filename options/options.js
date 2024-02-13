a4a0b93a96d866b77a784c53f01bbc5c9e802d1a
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 23:17:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-paper-dev-133413",
+  url: "https://falling-paper-dev-133413.botics.co",
+  api: "https://falling-paper-dev-133413.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
